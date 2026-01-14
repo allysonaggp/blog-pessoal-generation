28500bc335c7f1ee63fefb92aff8be35c6efcc24
allysonaggp <allysonaggp@gmail.com>
2026-01-14 13:36:33 -0300

    Criado a classe TemaService do modulo Service

---
+++ blogpessoal/src/tema/services/tema.services.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tema } from '../entities/tema.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TemaService {
+  constructor(
+    @InjectRepository(Tema)
+    private temaRepository: Repository<Tema>,
+  ) {}
+}
