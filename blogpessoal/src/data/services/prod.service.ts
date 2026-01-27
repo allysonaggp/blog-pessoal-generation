68d9877c779d6b1f8837b26d08f091029ce49b36
allysonaggp <allysonaggp@gmail.com>
2026-01-27 16:19:43 -0300

    Deploy do Projeto Blog Pessoal

---
+++ blogpessoal/src/data/services/prod.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProdService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      url: process.env.DATA_url,
+      logging: false,
+      ssl: { rejectUnauthorized: false },
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
+}
